f008bb1e8a2499d92d7c0428e9f32172dcecfec8
PoojaDivase <divasepooja@gmail.com>
2018-12-11 10:36:31 +0530

    demo1.java added

---
+++ src/git/tort/demo/demo1.java
@@ -0,0 +1,12 @@
+package git.tort.demo;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=10;
+		int c=a+b;
+		System.out.println("Addition = "+c);
+	}
+
+}
