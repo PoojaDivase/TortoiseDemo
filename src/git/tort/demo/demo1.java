cdda4b402be7e89e3de7a507e523e2f306f9a6fc
PoojaDivase <44114511+PoojaDivase@users.noreply.github.com>
2018-12-15 19:58:29 +0530

    remote master updated demo1

--- src/git/tort/demo/demo1.java
+++ src/git/tort/demo/demo1.java
@@ -4,13 +4,15 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10,b=10;
+		int a=10,b=2;
 		int c=a+b;
 		System.out.println("Addition = "+c);
 		c=a-b;
 		System.out.println("Subtraction = "+c);
 		c=a*b;
 		System.out.println("Multiplication = "+c);
+		c=a/b;
+		System.out.println("Division = "+c);
 	}
 
 }
